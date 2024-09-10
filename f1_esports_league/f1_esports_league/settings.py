bcf9fff2d4847e59760affcc7851df0f9b2c7883
James Curtis <jameswcurtis94@outlook.com>
2024-09-10 10:43:47 +0000

    .

--- f1_esports_league/f1_esports_league/settings.py
+++ f1_esports_league/f1_esports_league/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'f1_esports_league/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
